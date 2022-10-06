b67a0bfdc1a0d4e06c8cdd1e1be3ca1e505f7a10
Muhammed Ijlan <ijlanijlu580@gmail.com>
2022-10-06 12:07:16 +0530

    like a movie feature added to movie route

--- routes/movie.js
+++ routes/movie.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
 const verify = require("../verifyToken")
-const Movie = require("../models/Movie")
+const Movie = require("../models/Movie");
+const User = require("../models/User");
 
 // GET ALL
 router.get("/", async (req, res) => {
@@ -31,6 +32,17 @@ router.get("/:id", verify, async (req, res) => {
 })
 
 // FAVOURITE A MOVIE
-router.get("/like")
+router.put("/like/:id", verify, async (req, res) => {
+    try {
+        await User.findByIdAndUpdate(req.user.id, {
+            $push: { movieList: req.params.id }
+        })
+        res.status(201).json("Added to movie list")
+
+    } catch (e) {
+        console.log(e);
+        res.status(500).json({ error: e.message })
+    }
+})
 
 module.exports = router;
